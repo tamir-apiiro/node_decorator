039f8ae38bb98f5e92fc475bdda312dcd60566b2
tamir-apiiro <89914277+tamir-apiiro@users.noreply.github.com>
2025-02-11 11:58:26 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ const app = express()
 const port = 3000
 
 class yossi {
+    @LogWithPrefix2({some_flag: true, another_key: 7}, 'different value')
     @LogWithPrefix({some_flag: false, another_key: 2}, 'different value')
     blah(req: any, res: any) {
         res.send('Hello World!')
@@ -21,6 +22,16 @@ app.listen(port, () => {
 console.log('Happy developing ✨')
 
 function LogWithPrefix(options: any) {
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        const original = descriptor.value;
+        descriptor.value = function (...args: any[]) {
+            console.log(`${options.some_flag} ${propertyKey} called with ${args}`);
+            return original.apply(this, args);
+        };
+    };
+}
+
+function LogWithPrefix2(options: any) {
     return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
         const original = descriptor.value;
         descriptor.value = function (...args: any[]) {
